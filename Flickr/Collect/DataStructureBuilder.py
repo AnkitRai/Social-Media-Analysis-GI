783c119965f08996690e9b37293de2c88176887e
EasonSun <Account@wirelessprvnat-172-17-195-28.near.illinois.edu>
2015-06-18 19:01:19 -0500

    Raw image and text
    MostViewedCrawler.py: crawler for Flickr data
    renumber.py: after manually clean, number the data from 1 to the end
    DataStructureBuilder: build heterogeneous data structure

---
+++ Flickr/Collect/DataStructureBuilder.py
@@ -0,0 +1,12 @@
+
+'''
+The data is expected to be stored in a 2D data structure, where 
+the first index is over features and the second is over samples.
+
+data = {'img': ['1.jpg', '2.jpg', '3.jpg'],
+		'comment': ['Great!', 'Nice shot', 'Awsome'],
+		'view': [301, 222, 256],
+		'fav': [2, 3, 4],
+		'cmt': [11, 15, 16]
+}
+'''
